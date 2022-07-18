b813fd027bf5566ebff3389fc0743b7c3ff831cc
andreadacy <andreadacy@gmail.com>
2022-07-18 06:15:23 -0700

    completed challenge

--- Challenge/static/js/challenge_logic.js
+++ Challenge/static/js/challenge_logic.js
@@ -108,6 +108,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         },
       // We set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
+    
      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
      //  after the marker has been created and styled.
      onEachFeature: function(feature, layer) {
@@ -211,6 +212,7 @@ legend.onAdd = function() {
       style: {color: "#703606", weight: 3},
     }).addTo(tectonicPlates); 
   });
+
   // Add the tectonic layer group to the map.
   tectonicPlates.addTo(map);
 });
